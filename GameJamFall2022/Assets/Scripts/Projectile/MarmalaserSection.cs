825e4acced93a7369ed47823455017b78efd684e
EclipsesShadow <bencol1022@gmail.com>
2022-11-12 15:43:59 -0600

    marmalaser finsished

--- GameJamFall2022/Assets/Scripts/Projectile/MarmalaserSection.cs
+++ GameJamFall2022/Assets/Scripts/Projectile/MarmalaserSection.cs
@@ -1,32 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class MarmalaserSection : MonoBehaviour
 {
     public List<Sprite> sprites;
     public List<Sprite> splash;
-    public int count = 5;
+    public int count;
+    public Vector2 dir;
+    private int index;
 
     // Start is called before the first frame update
     void Start()
     {
+        index = count % 3;
         gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
         if (count > 0)
         {
-            Vector2 dir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - gameObject.transform.position);
+            if(this.dir.Equals(new Vector2()))
+            {
+                dir = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - gameObject.transform.position);
+                gameObject.transform.Rotate(new Vector3(0, 0, Vector3.SignedAngle(new Vector3(1, 0, 0), dir, Vector3.forward)));
+                dir.Normalize();
+            }
             GameObject obj = GameObject.Instantiate(gameObject);
-            dir.Normalize();
+            obj.GetComponent<MarmalaserSection>().dir = this.dir;
             obj.GetComponent<MarmalaserSection>().count = count - 1;
             obj.transform.position = gameObject.transform.position + (new Vector3(dir.x, dir.y, 0) * 0.9f);
-            obj.transform.Rotate(new Vector3(0, 0, Vector3.Angle(new Vector3(0, 1, 0), dir)));
-
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[Random.Range(0, sprites.Count-1)];
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[index];
+        index++;
+        index %= 3;
     }
 }
