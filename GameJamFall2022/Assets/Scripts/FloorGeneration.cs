a46c074b7a4f982f3d0b33db7f7cfe4bf25f7a64
EclipsesShadow <bencol1022@gmail.com>
2022-11-12 14:43:37 -0600

    floor

--- GameJamFall2022/Assets/Scripts/FloorGeneration.cs
+++ GameJamFall2022/Assets/Scripts/FloorGeneration.cs
@@ -17,11 +17,19 @@ public class FloorGeneration : MonoBehaviour
         Tilemap floor = gameObject.transform.GetComponentInChildren<Tilemap>();
         floor.ClearAllTiles();
 
-        for (int i = sizeX; i < sizeX; i++)
+        for (int i = -sizeX; i < sizeX; i++)
         {
             for (int j = -sizeY; j < sizeY; j++)
             {
-                floor.SetTile(new Vector3Int(i, j, 0), tiles[Random.Range(0, tiles.Count-1)]);
+                if (Random.Range(0, 10) < 9)
+                {
+                    floor.SetTile(new Vector3Int(i, j, 0), tiles[((i + j) % 2 == 0) ? 0: (tiles.Count) / 2]);
+                }
+                else
+                {
+                    floor.SetTile(new Vector3Int(i, j, 0), tiles[((i + j) % 2 == 0) ? Random.Range(0, (tiles.Count - 1) / 2) : Random.Range((tiles.Count - 1) / 2, tiles.Count - 1)]);
+
+                }
             }
         }
     }
