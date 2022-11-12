738355a68ca793b1aacf562cd436a1788c5b4fe1
slastine <115110772+slastine@users.noreply.github.com>
2022-11-12 17:22:53 -0600

    loafmother

--- GameJamFall2022/Assets/Scripts/Projectile/Projectile.cs
+++ GameJamFall2022/Assets/Scripts/Projectile/Projectile.cs
@@ -2,20 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
-[RequireComponent(typeof(Rigidbody))]
 public class Projectile : MonoBehaviour
 {
-    public Rigidbody2D rb;
     public float damage;
     public bool destroyOnHit;
-    public BoxCollider2D bc;
     // Start is called before the first frame update
-    void Start()
-    {
-        rb = this.gameObject.GetComponent<Rigidbody2D>();
-        bc = this.gameObject.GetComponent<BoxCollider2D>();
-    }
+
 
     
 }
