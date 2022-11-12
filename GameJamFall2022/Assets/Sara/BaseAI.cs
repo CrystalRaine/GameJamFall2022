04f492c06095194cc59824793a58cc779efb97da
slastine <115110772+slastine@users.noreply.github.com>
2022-11-12 11:34:26 -0600

    ghfgh

--- GameJamFall2022/Assets/Sara/BaseAI.cs
+++ GameJamFall2022/Assets/Sara/BaseAI.cs
@@ -1,44 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseAI : MonoBehaviour
 {
-    public GameObject circle;
+    public GameObject enemy;
     Vector3 currentGoal;
+
+    public float maxX = 10;
+    public float maxY = 4.5f;
+    public float minX = -10;
+    public float minY = -4.5f;
+    public float movementRange = 3;
+    public float step = .005f;
+
     private void Start()
     {
         currentGoal = GetRandomPositionNearSelf();
     }
     public void MoveToGoal(Vector2 goal)
     {
-        circle.transform.position = Vector2.MoveTowards(circle.transform.position, goal, .005f);
+        enemy.transform.position = Vector2.MoveTowards(enemy.transform.position, goal, step);
     }
 
     public Vector3 GetRandomPositionNearSelf()
     {
-        Vector3 random = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
-        random = new Vector3(circle.transform.position.x + random.x, circle.transform.position.y + random.y, 0);
-        if(random.x <= -10) { random.x = -10; }
-        if(random.x >= 10) { random.x = 10; }
-        if(random.y <= -5) { random.y = -4.5f; }
-        if(random.y >= 5) { random.y = 4.5f; }
+        Vector3 random = new Vector3(UnityEngine.Random.Range(-movementRange, movementRange), UnityEngine.Random.Range(-movementRange, movementRange));
+        random = new Vector3(enemy.transform.position.x + random.x, enemy.transform.position.y + random.y, 0);
+        if(random.x <= minX) { random.x = minX; }
+        if(random.x >= maxX) { random.x = maxX; }
+        if(random.y <= minY) { random.y = minY; }
+        if(random.y >= maxY) { random.y = maxY; }
         return random;
     }
 
+    public bool NearPlayer(float radius)
+    {
+        if (Vector3.Distance(new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0), enemy.transform.position) < radius) { Debug.Log("True");  return true; } else { return false; }
+    }
     // Update is called once per frame
     void Update()
     {
+        if (NearPlayer(8))
+        {
+            currentGoal = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+        }
         
-        if (circle.transform.position != currentGoal)
+        if (Vector3.Distance(enemy.transform.position, currentGoal) >= .001)
         {
             MoveToGoal(currentGoal);
         }
-        else
+        else if(Vector3.Distance(enemy.transform.position, currentGoal) <= .001 && !NearPlayer(8))
         {
-            currentGoal = GetRandomPositionNearSelf();
 
+            currentGoal = GetRandomPositionNearSelf();
         }
-        
+
     }
 }
--- GameJamFall2022/Assets/Sara/Spawner.cs
+++ GameJamFall2022/Assets/Sara/Spawner.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public GameObject spawnType;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("Spawn", 0, 5);
     }
 
     public void Spawn() 
-    { 
-        
+    {
+        GameObject go = GameObject.Instantiate(spawnType);
     }
 }
