b6f2ef9c40339bab7854873bde222b81bfe35b62
slastine <115110772+slastine@users.noreply.github.com>
2022-11-12 14:46:37 -0600

    ghfd

--- GameJamFall2022/Assets/Scripts/Projectile/GrapeshotCreator.cs
+++ GameJamFall2022/Assets/Scripts/Projectile/GrapeshotCreator.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GrapeshotCreator : MonoBehaviour
+public class GrapeshotCreator : Projectile
 {
     public GameObject particle;
     public int count;
