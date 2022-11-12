1cf7541f4c977671ed89b55f8a7c23dac2b3f8e8
slastine <115110772+slastine@users.noreply.github.com>
2022-11-11 20:22:46 -0600

    hjgfds

--- GameJamFall2022/Assets/Sara/MoveTest.cs
+++ GameJamFall2022/Assets/Sara/MoveTest.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class MoveTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject circle;
+
+    public void MoveToGoal(Vector2 goal)
     {
-        
+        Vector2.MoveTowards(circle.transform.position, goal, .1f);
+    }
+
+    public Vector2 GetRandomPositionNearSelf()
+    {
+        Vector2 random = new Vector2(Random.Range(0, .01f), Random.Range(0, .1f));
+        random = new Vector2(circle.transform.position.x + random.x, circle.transform.position.y + random.y);
+        return random;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveToGoal(GetRandomPositionNearSelf());
     }
 }
