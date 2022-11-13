b0b3b32de6aa8f8373d0f36057c952588dea25f1
EclipsesShadow <bencol1022@gmail.com>
2022-11-13 09:41:22 -0600

    add scenes to build

--- GameJamFall2022/Assets/Scripts/MenuScripts/StartGame.cs
+++ GameJamFall2022/Assets/Scripts/MenuScripts/StartGame.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+
+    public string goesTo;
     public void OnButtonPress()
     {
-        Debug.Log("click");
+        SceneManager.LoadScene(goesTo);
     }
 }
