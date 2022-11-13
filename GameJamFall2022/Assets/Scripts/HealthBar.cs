5253353333b5923808c9f4ec472cbec3019ff757
TheShauceGod <bdiaz9@iastate.edu>
2022-11-13 15:09:30 -0600

    ffg

--- GameJamFall2022/Assets/Scripts/HealthBar.cs
+++ GameJamFall2022/Assets/Scripts/HealthBar.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using TMPro;
+using UnityEngine;
+
 public class HealthBar : MonoBehaviour
 {
     public TMP_Text text;
