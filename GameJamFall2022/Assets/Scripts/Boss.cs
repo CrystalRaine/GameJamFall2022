cd22e5ddb9ba9e3eb416fd4f454cc4122b9618ff
slastine <115110772+slastine@users.noreply.github.com>
2022-11-13 17:12:47 -0600

    gjg

--- GameJamFall2022/Assets/Scripts/Boss.cs
+++ GameJamFall2022/Assets/Scripts/Boss.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Boss : MonoBehaviour
 {
-    public GameObject bossLegRight;
-    public GameObject bossLegLeft;
-    public GameObject bossHandRight;
-    public GameObject bossHandLeft;
-    public int legHealth;
+    public GameObject[] prefabs;
+    public GameObject bossLeg;
+    public GameObject bossHand;
+    public float legHealth = 5;
     void Awake()
     {
+        bossLeg = GameObject.Instantiate(prefabs[0]);
+        bossLeg.GetComponent<BossLeg>().interval = 4;
+        bossLeg.GetComponent<BossLeg>().Begin();
+        bossLeg.GetComponent<BossLeg>().boss = this;
+
         
     }
 
-    
+    public void CreateHand()
+    {
+        bossHand = GameObject.Instantiate(prefabs[1]);
+        bossHand.GetComponent<BossLeg>().interval = 2;
+        bossHand.GetComponent<BossLeg>().boss = this;
+        bossHand.GetComponent<BossLeg>().Begin();
+        legHealth = 30;
+    }
+
+    private void Update()
+    {
+        if(legHealth <= 0 && bossHand == null) 
+        {
+            if(bossLeg != null && bossHand == null) 
+            {
+                bossLeg.GetComponent<BossLeg>().Die();
+                
+            }
+            else 
+            {
+                CreateHand();
+            }
+
+        }
+        else if(legHealth <= 0) 
+        {
+            if (bossHand != null)
+            {
+                bossHand.GetComponent<BossLeg>().Die();
+                Debug.Log("You win!");
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
 }
